a04f26608f7af6f2c3bea357d917c759d28a42bc
Daniel <dca>
2021-01-30 20:01:08 -0600

    prueba numero uno
    se creo ahora

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Login | Sistema Facturación</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<section id="container">
+		<form action="" method="post">
+			<h3>Iniciar Sesión</h3>
+			<img src="img/login.png" alt="Login">
+
+			<input type="text" name="usuario" placeholder="Usuario">
+			<input type="password" name="clave" placeholder="Contraseña">
+			<div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
+			<input type="submit" value="INGRESAR">
+
+		</form>
+
+	</section>
+</body>
+</html>
